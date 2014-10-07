f23b83a71e488389686a1bd880c15c1c6ab25f9f
Shreyas <skraman1999@gmail.com>
2014-10-07 00:51:13 -0700

    Better background color.

--- core/src/game/tile/arena/TileArena.java
+++ core/src/game/tile/arena/TileArena.java
@@ -37,7 +37,7 @@ public class TileArena extends ApplicationAdapter {
             o.update(delta);
         last = temp;
 
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 

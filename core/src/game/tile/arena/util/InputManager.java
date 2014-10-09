19fd2c2035fbc8883f1f1e93b31f23a9ead42121
Shreyas <skraman1999@gmail.com>
2014-10-08 20:02:54 -0700

    Added held.

--- core/src/game/tile/arena/util/InputManager.java
+++ core/src/game/tile/arena/util/InputManager.java
@@ -7,16 +7,19 @@ public class InputManager {
     private static boolean touched;
     private static boolean released;
     private static boolean pressed;
+    private static boolean held;
 
     public static boolean isTouched() { return touched; }
     public static boolean isReleased() { return released; }
-    public static boolean isPressed() { return pressed;  }
+    public static boolean isPressed() { return pressed; }
+    public static boolean isHeld() { return held; }
 
     public static void update() {
         boolean nTouched = Gdx.input.isTouched();
 
-        pressed = nTouched && !isTouched();
-        released = isTouched() && !nTouched;
+        pressed = nTouched && !touched;
+        released = touched && !nTouched;
+        held = nTouched && touched;
 
         touched = nTouched;
     }

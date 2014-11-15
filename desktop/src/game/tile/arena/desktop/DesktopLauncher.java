ca40f831c2fd1eb4e621728b5c4c7116181c9caf
Shreyas <skraman1999@gmail.com>
2014-11-15 13:22:25 -0800

    Added desktop and HTML projects.

---
+++ desktop/src/game/tile/arena/desktop/DesktopLauncher.java
@@ -0,0 +1,13 @@
+package game.tile.arena.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import game.tile.arena.TileArena;
+
+public class DesktopLauncher {
+
+	public static void main (String[] args) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TileArena(), config);
+	}
+}
